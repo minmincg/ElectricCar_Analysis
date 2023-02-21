e1a82191df14eb8aef5f297212a82d927f4ab4a0
javrobs <javieroblesamar@gmail.com>
2023-02-21 15:35:40 +0800

    images, maps and results added

--- Analysis/functions.py
+++ Analysis/functions.py
@@ -14,7 +14,9 @@ def heatmap(df,state_column_name,outputcolumn,cmap="plasma"):
     exclude=['Commonwealth of the Northern Mariana Islands',
          'Puerto Rico','Guam','American Samoa','United States Virgin Islands']
     states=states.loc[states["name"].isin(exclude)==False]
-    merge_with=df[[state_column_name,outputcolumn]]
+    states["name"]=states["name"].str.upper()
+    merge_with=df[[state_column_name,outputcolumn]].copy()
+    merge_with[state_column_name]=merge_with[state_column_name].str.upper()
     states=pd.merge(states,merge_with,how="inner",left_on="name", right_on=state_column_name)
     map1=states.hvplot.polygons(geo=True,
                                 tiles=True,
@@ -35,14 +37,17 @@ def regression(df,col1,col2):
     
     model = st.linregress(df[col1], df[col2])
     r_squared = model.rvalue**2
-    print(f"The correlation between {col1} and {col2} is {model.rvalue:.2f} with a p-value of {model.pvalue:.2f}")
+    print(f"The correlation between {col1} and {col2} is {model.rvalue:.2f} with a p-value of {model.pvalue}")
     y_values = df[col1]*model[0]+model[1]
     plt.figure(figsize=(10, 7))
     plt.scatter(df[col1],df[col2])
     plt.plot(df[col1],y_values,color="red")
     plt.xlabel(col1)
     plt.ylabel(col2)
+    plt.title(f"{col1} VS {col2}")
+    plt.grid(True)
+    plt.savefig(f"../Images/{col1.replace(' ','_').replace('/','')}_{col2.replace(' ','_').replace('/','')}_corr.png")
     plt.show()
     print(f"The r-squared value of the linear regression model is {r_squared:.2f}")
     
-    return
+    return model.rvalue
